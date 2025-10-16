0ec9ee775d34e35c64cc668305eca39d4018fca7
emery.tossavi <emerytossavi@gmail.com>
2025-10-16 15:54:59 +0100

    [ADD] : shortLink's table && model

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function ShortLinks(){
+        return $this->hasMany(User::class, 'user_id');
+    }
 }
