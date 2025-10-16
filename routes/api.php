4d581a60e73d63f97afafde8702c20be1157f9a3
emery.tossavi <emerytossavi@gmail.com>
2025-10-16 16:06:04 +0100

    [REFACTOR] : convert activate and deactivate to post methods

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,11 @@ Route::get('/user', function (Request $request) {
     Route::get("/modules", [ModuleController::class, 'index']);
 Route::middleware(['CheckModuleActive'])->prefix('modules/{id}')->group(function () {
 
-    Route::get("/deactivate", [UserModuleController::class, "deActivateModule"])->name("module.deActivate");
+    Route::post("/deactivate", [UserModuleController::class, "deActivateModule"])->name("module.deActivate");
 
 });
 
-Route::get("modules/{id}/activate", [UserModuleController::class, "activateModule"])->name("module.activate");
+Route::post("modules/{id}/activate", [UserModuleController::class, "activateModule"])->name("module.activate");
 
 Route::post("/register", [UserController::class, "register"])->name('user.register');
 Route::post("/login", [UserController::class, "login"])->name('user.login');
